fbb00373777fa08082eac5a204ee746a5507c444
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 21:51:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,7 +149,8 @@ end
 def team_colors(team_name)
   game_hash.each do |team, team_data|
     if team_data[:team_name] == team_name
-      team_data[:colors].values
+      binding.pry
+      return team_data[:colors]
     end
   end
 end

9c5c104e9e7aeb4b76b5540b68b7084a8d33025c
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 20:45:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -90,25 +90,25 @@ def game_hash
           :blocks = 5,
           :slam_dunks = 5
         },
-        "Mason Plumlee" => {
-          :number = "1",
-          :shoe = 19,
-          :points = 26,
-          :rebounds = 12,
-          :assists = 6,
-          :steals = 3,
-          :blocks = 8,
-          :slam_dunks = 5
-        },
-        "Jason Terry" => {
-          :number = "31",
+        "Ben Gordon" => {
+          :number = "8",
           :shoe = 15,
-          :points = 19,
-          :rebounds = 2,
+          :points = 33,
+          :rebounds = 3,
           :assists = 2,
-          :steals = 4,
-          :blocks = 11,
-          :slam_dunks = 1
+          :steals = 1,
+          :blocks = 1,
+          :slam_dunks = 0
+        },
+        "Brendan Haywood" => {
+          :number = "33",
+          :shoe = 15,
+          :points = 6,
+          :rebounds = 12,
+          :assists = 12,
+          :steals = 22,
+          :blocks = 5,
+          :slam_dunks = 12
         }
       }
     }

97b20ef5dd2e6539703992fc86823cfee9fdc327
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 21:21:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -124,6 +124,7 @@ def num_points_scored(name)
       if attribute == :players
         values.each do |player, stats|
           if name == player
+            binding.pry
             stats[:points]
           end
         end

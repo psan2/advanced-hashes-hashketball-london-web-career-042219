148051082ab8502a027b92f757cbca451c89d350
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 20:57:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,20 +118,9 @@ def game_hash
   }
 end
 
-def good_practices
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    binding.pry
-      team_data.each do |attribute, data|
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-        binding.pry
-
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-        data.each do |data_item|
-            binding.pry
-      end
-    end
-  end
+def num_points_scored(name)
+  game_hash.each do |team, team_data|
+    team_data.each do |player, player_stats|
+      if player == name
+        [:points]
 end
-
-good_practices

69b6dbbacd49efdd9ac0d7b8e5894d58ab5bb8ff
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 21:59:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -165,7 +165,7 @@ end
 def player_numbers(team)
   number_array = []
   game_hash.each do |team, team_data|
-    if team_data[:team_name] == name
+    if team_data[:team_name] == team
       team_data.each do |attribute, values|
         values.each do |player, stats|
           number_array << stats[:number]

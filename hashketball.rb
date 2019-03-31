499b723f2f4900dc81581c484d10ce656d8ce71b
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 20:44:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -60,7 +60,56 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turqoise","Purple"],
       :players => {
-
+        "Jeff Adrien" => {
+          :number = "4",
+          :shoe = 18,
+          :points = 10,
+          :rebounds = 1,
+          :assists = 1,
+          :steals = 2,
+          :blocks = 7,
+          :slam_dunks = 2
+        },
+        "Bismak Biyombo" => {
+          :number = "0",
+          :shoe = 16,
+          :points = 12,
+          :rebounds = 4,
+          :assists = 7,
+          :steals = 7,
+          :blocks = 15,
+          :slam_dunks = 10
+        },
+        "DeSagna Diop" => {
+          :number = "2",
+          :shoe = 14,
+          :points = 24,
+          :rebounds = 12,
+          :assists = 12,
+          :steals = 4,
+          :blocks = 5,
+          :slam_dunks = 5
+        },
+        "Mason Plumlee" => {
+          :number = "1",
+          :shoe = 19,
+          :points = 26,
+          :rebounds = 12,
+          :assists = 6,
+          :steals = 3,
+          :blocks = 8,
+          :slam_dunks = 5
+        },
+        "Jason Terry" => {
+          :number = "31",
+          :shoe = 15,
+          :points = 19,
+          :rebounds = 2,
+          :assists = 2,
+          :steals = 4,
+          :blocks = 11,
+          :slam_dunks = 1
+        }
       }
     }
   }

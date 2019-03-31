5e5032c663b08c6f02241ad2c7508cfe0fb6b336
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-31 22:08:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -180,6 +180,7 @@ def player_stats(player_name)
       if attribute == :players
         values.each do |player, stats|
           if player_name==player
+            stats[:number].map! {|number| number.to_i}
             return stats
           end
         end
